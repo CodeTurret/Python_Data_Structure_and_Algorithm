023c1388b6c185ea0cee88d3ae8d9cda53771f8e
CodeTurret <mushfiq.fuad@gmail.com>
2018-11-06 22:22:39 +0600

    Array operations in Python

---
+++ Array/Array.py
@@ -0,0 +1,12 @@
+#Declare an array
+
+from array import *
+
+ #arrayName = array(typecode, [initializer])
+
+arrayName = array('i', [10,20,21,22,45,32])
+
+ #print array
+
+for i in arrayName:
+     print(i)
