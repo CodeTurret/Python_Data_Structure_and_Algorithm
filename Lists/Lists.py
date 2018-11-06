f9969681a8f9ebc3dc8958f576dab1f858035b54
CodeTurret <mushfiq.fuad@gmail.com>
2018-11-06 23:03:29 +0600

    Lists Introduction

---
+++ Lists/Lists.py
@@ -0,0 +1,4 @@
+#The list is a most versatile datatype available in Python
+#which can be written as a list of comma-separated values (items) between square
+#brackets. Important thing about a list is that items in a list need not be of
+#the same type.
